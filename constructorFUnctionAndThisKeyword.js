2a386c8135b8e4f9f4d42420ad43243923f5294f
tomonakar <tomohisa.nak@gmail.com>
2018-06-18 13:27:06 +0900

    prototypeにアクセス

--- constructorFUnctionAndThisKeyword.js
+++ constructorFUnctionAndThisKeyword.js
@@ -6,4 +6,14 @@ function User(firstName, lastName, age, gender) {
 }
 
 const user1 = new User('tomo', 'nakar', 35, 'male')
+const user2 = new User('hoge', 'fuga', 21, 'female')
 console.log(user1)
+console.log(user2)
+
+// prototypeにemailDomainを追加
+User.prototype.emailDomain = '@faicebook.com'
+
+// prototypeにアクセス
+console.log(user1.__proto__)
+console.log(user2.__proto__)
+console.log(user2.emailDomain)
