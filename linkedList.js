0f7a610aa72d547025c93dfbedd55b20b8074185
tomonakar <tomohisa.nak@gmail.com>
2018-06-19 23:36:20 +0900

    feat: linked listを追加

---
+++ linkedList.js
@@ -0,0 +1,13 @@
+function LinkedList() {
+  this.head = null
+  this.tail = null
+}
+
+function Node(value, next, prev) {
+  this.value = value
+  this.next = next
+  this.prev = prev
+}
+
+const node1 = new Node(100, 'node2', null)
+console.log(node1)
