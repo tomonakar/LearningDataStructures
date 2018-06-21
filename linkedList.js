e2d0c91c925d7abb3fda6fdf3dd2a4f13a827ac5
tomonakar <tomohisa.nak@gmail.com>
2018-06-21 14:17:32 +0900

    linkedListにtail,headerの追加・削除メソッドを追加

--- linkedList.js
+++ linkedList.js
@@ -9,9 +9,47 @@ function Node(value, next, prev) {
   this.prev = prev
 }
 
-LinkedList.prototype.addToHead = function (value) {
+LinkedList.prototype.addToHead = function(value) {
   var newNode = new Node(value, this.head, null)
   if (this.head) this.head.prev = newNode
   else this.tail = newNode
   this.head = newNode
 }
+
+LinkedList.prototype.addToTail = function(value) {
+  var newNode = new Node(value, null, this.tail)
+  if(this.tail) this.tail.next = newNode
+  else this.head = newNode
+  this.tail = newNode
+}
+
+LinkedList.prototype.removeHead = function() {
+  if (!this.head) return null
+  var val = this.head.value
+  this.head = this.head.next
+  if (this.head) this.head.prev = null
+  else this.tail = null
+  return val
+}
+
+LinkedList.prototype.removeTail = function() {
+  if (!this.tail) return null
+  var val = this.tail.value
+  this.taile = this.tail.prev
+  if (this.tail) this.tail.prev = null
+  else this.tail = null
+  return val
+}
+
+
+
+var ll = new LinkedList()
+
+ll.addToHead('one')
+ll.addToHead('two')
+ll.addToHead('three')
+
+
+ll.removeHead()
+
+console.log(ll);
