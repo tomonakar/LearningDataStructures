4461dbcdd3968bb2a1f218c2204ce1bdebd1d777
tomonakar <tomohisa.nak@gmail.com>
2018-06-22 09:28:48 +0900

    feat: serchメソッド追加

--- linkedList.js
+++ linkedList.js
@@ -10,14 +10,14 @@ function Node(value, next, prev) {
 }
 
 LinkedList.prototype.addToHead = function(value) {
-  var newNode = new Node(value, this.head, null)
+  const newNode = new Node(value, this.head, null)
   if (this.head) this.head.prev = newNode
   else this.tail = newNode
   this.head = newNode
 }
 
 LinkedList.prototype.addToTail = function(value) {
-  var newNode = new Node(value, null, this.tail)
+  const newNode = new Node(value, null, this.tail)
   if(this.tail) this.tail.next = newNode
   else this.head = newNode
   this.tail = newNode
@@ -25,7 +25,7 @@ LinkedList.prototype.addToTail = function(value) {
 
 LinkedList.prototype.removeHead = function() {
   if (!this.head) return null
-  var val = this.head.value
+  const val = this.head.value
   this.head = this.head.next
   if (this.head) this.head.prev = null
   else this.tail = null
@@ -34,22 +34,29 @@ LinkedList.prototype.removeHead = function() {
 
 LinkedList.prototype.removeTail = function() {
   if (!this.tail) return null
-  var val = this.tail.value
+  const val = this.tail.value
   this.taile = this.tail.prev
   if (this.tail) this.tail.prev = null
   else this.tail = null
   return val
 }
 
+LinkedList.prototype.search = function(searchValue) {
+  let currentNode = this.head
+  while(currentNode) {
+    if (currentNode.value === searchValue) return currentNode.value
+    currentNode = currentNode.next
+  }
+  return null
 
+}
 
-var ll = new LinkedList()
+const ll = new LinkedList()
+ll.addToHead(123)
+ll.addToHead(70)
+ll.addToHead('hello')
+ll.addToTail(19)
+ll.addToTail('world')
+ll.addToTail(20)
 
-ll.addToHead('one')
-ll.addToHead('two')
-ll.addToHead('three')
-
-
-ll.removeHead()
-
-console.log(ll);
+console.log(ll.search('hello'))
