241b6a0e69fa02ad54ef6087bf8981ee12e2461c
tomonakar <tomohisa.nak@gmail.com>
2018-06-25 16:38:57 +0900

    feat: ハッシュ作成メソッドを追加

--- hashTable.js
+++ hashTable.js
@@ -3,11 +3,21 @@ function HashTable(size) {
   this.numBuckets = this.buckets.length
 }
 
-const myHT = new HashTable(30)
-console.log(myHT)
-
 function HashNode(key, value, next) {
   this.key = key
   this.value = value
   this.next = next || null
 }
+
+HashTable.prototype.hash = function(key) {
+  let total = 0
+  for (let i = 0; i < key.length; i++) {
+    total += key.charCodeAt(i)
+  }
+  let bucket = total % this.numBuckets
+  return bucket
+}
+
+let myHT = new HashTable(30)
+console.log(myHT.hash('Becca'))
+console.log(myHT)
